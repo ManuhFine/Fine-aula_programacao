fad149ffa2e42d340d06cfd4e86ccd2b44bfb4a5
Manuh Fine <82736056+ManuhFine@users.noreply.github.com>
2025-06-16 13:54:30 -0300

    calculando_media.c

--- calculando_media.c
+++ calculando_media.c
@@ -1,5 +1,6 @@
 //Simulando um calculo básico de notas
-//Crie um algoritmo que leia 3 notas e calcule a média entre elas.
+//Crie um algoritmo que leia 2 notas e calcule a média entre elas, sendo que, cada uma tem um peso de 5.
+//Opcional = Baseando-se em uma média de 7.0, faça com que imprima se o aluno foi aprovado ou reprovado.
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,18 +9,18 @@
 void main(){
     setlocale(LC_ALL, "");
  
-    float nota1, nota2, nota3, resultado; //define as variaveis
+    float nota1, nota2, resultado; //define as variaveis
  
-    printf("Digite a primeira nota:\n"); //imprime na tela pedindo para digitar a 1°nota
-    scanf("%f", &nota1); //lê e salva o digito.
+    printf("Digite as 2 notas:\n"); //imprime na tela pedindo para digitar a 1°nota
+    scanf("%f %f", &nota1, &nota2); //lê e salva o digito.
 
-    printf("Digite a segunda nota:\n"); //imprime na tela pedindo para digitar a 2°nota
-    scanf("%f", &nota2); //lê e salva o digito.
+    resultado = ((nota1*5) + (nota2*5) ) / 10; //Aqui, ele vai somar as notas e depois dividir, para tirar a média
+    printf("A média é: %.1f", resultado); // Aqui, ele ja imprime o resultado da soma, sem mostrar o calculo (%.1f = mostrar apenas aquele numero de casa decimais).
     
-    printf("Digite a terceira nota:\n"); //imprime na tela pedindo para digitar a 3°nota
-    scanf("%f", &nota3); //lê e salva o digito.
-
-    resultado = (nota1 + nota2 + nota3) / 3; //Aqui, ele vai somar as notas e depois dividir, para tirar a média
-    printf("A média é: %f", resultado); // Aqui, ele ja imprime o resultado da soma, sem mostrar o calculo.
+    if (resultado >= 7.0) {
+    printf("\nAluno aprovado!\n");
+} else {
+    printf("\nAluno reprovado.\n");
+}
  
 }
