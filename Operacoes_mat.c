436118955962e1f7973a2eada3de5e2fda5ad2cb
Manuh Fine <82736056+ManuhFine@users.noreply.github.com>
2025-06-16 13:17:26 -0300

    Operacoes_mat.c

--- Operacoes_mat.c
+++ Operacoes_mat.c
@@ -1,33 +1,40 @@
-//para lembrar as operações matemáticas
+//Para lembrar...
+//Crie variaveis, onde cada uma, faça uma operação matematica com os valores digitados.
 
-#include <stdio.h>
-#include <stdlib.h>
- 
+#include <stdio.h> //Fornece funções para entrada e saida de dados.
+#include <stdlib.h> //Fornece funções de uso geral, especialmente relacionadas a: gerenciamento de memoria, conversão de tipos, controle de processos.
+#include <locale.h> //Permite configurar o idioma, formatação de números, datas, e acentuação de acordo com a localidade (região/país).
+
 //Função principal do programa
-void main(){
- 
-    //Definindo Variáveis
-    int a = 6, b = 4;
- 
-    //Soma
-    printf("\n A soma de %d e %d = %d",a , b, a + b);
- 
-    //Subtração
-    printf("\n A subtracao de %d e %d = %d",a , b, a - b);
- 
-    //Divisão
-    printf("\n A divisao de %d e %d = %d",a , b, a / b);
- 
-    //Multiplicação
-    printf("\n A multiplicacao de %d e %d = %d",a , b, a * b);
- 
-    //Resto da Divisão
-    printf("\n O resto da divisao entre %d e %d = %d",a , b, a % b);
- 
-    //Valor Absoluto
-    printf("\n O valor absoluto de -3 = %d", abs(-3));
- 
-    //Pausa o programa após executar
-    system("pause");
- 
+void main() { 
+    
+    //Para configurar a linguagem
+    setlocale(LC_ALL,"");
+    
+    //Define as variaveis:
+    int a, b;
+    
+	//Pede para o usuario digitar valores para as variaveis
+	printf("Para fazer os diversos calculos, digite apenas numeros inteiros\n"); //imprime na tela, 
+	printf("Digite o primeiro valor: "); //pedindo para digitar um numero.
+    scanf("%d", &a); //lê e salva o digito.
+    printf("Digite o segundo valor: "); 
+    scanf("%d", &b);
+
+    //Aqui, o sistema vai fazer os calculos e imprimir os resultado
+	//Soma
+	printf("\n A soma de %d e %d = %d",a, b, a + b);
+
+	//Subtração
+	printf("\n A subtracao de %d e %d = %d",a, b, a - b);
+
+	//Divisão
+	printf("\n A divisao de %d e %d = %d",a, b, a / b);
+
+	//Multiplicação
+	printf("\n A multiplicacao de %d e %d = %d",a, b, a * b);
+
+	//Pausa o programa após executar
+	system("pause");
+
 }
